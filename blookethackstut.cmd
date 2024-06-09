e68ed4ec42cb45499fa05e9fb7c6315a80e874f3
J2050sMods <jjleon2015@icloud.com>
2024-06-08 20:08:55 -0700

    yippee it works

---
+++ blookethackstut.cmd
@@ -0,0 +1,21 @@
+echo off
+color 0a
+title Blooket GUI Inserting Tutorial
+:start
+echo Firstly copy the code, to do so Click the little clipboard with a square
+pause
+echo After that go to blooket
+pause
+echo Then Right Click and choose Inspect
+pause
+echo Next to Inspect Click the little square with "greater_than underscore"
+pause
+echo Then CTRL+V the code in. If it says [ allow pasting ] just type "Allow Pasting" and try again.
+pause
+echo then enter and there you have it! Your GUI/Mod is working!
+pause
+Press Any key to exit!
+pause
+exit
+
+
